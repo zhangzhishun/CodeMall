f06454bd2989bb2964b139a169e99e99d22b1d53
zhangzhishun <hebeidaxue2016>
2019-11-04 21:41:47 +0800

    Springboot+thymeleaf

---
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/index")
+    public String index(Model model){
+        String test = "test123";
+        System.out.println(test);
+        model.addAttribute("test",test);
+        return "index";
+    }
+
+
+}
